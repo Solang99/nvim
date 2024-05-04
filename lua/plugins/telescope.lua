ac9a650ea31ce4892b0b02519e5cf48e0ea9dc02
Pio Angelo Solimeno <angelo.solimeno@kelyon.com>
2024-05-04 10:25:36 +0200

    remove packer leftovers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<c-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>th", builtin.colorscheme , {})
 		end,
--- lua/vim-option.lua
+++ lua/vim-option.lua
@@ -1,5 +1,14 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+
+-- Normal mode: save
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+
+-- Insert mode: escape to normal and save
+vim.api.nvim_set_keymap('i', '<C-s>', '<Esc>:w<CR>l', { noremap = true, silent = true })
+
+-- Visual mode: escape to normal and save
+vim.api.nvim_set_keymap('v', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true })
